93c52ab85aa48f16f62200b7bd8eb8f6a20c34f5
Brett Newlund <bmnewlund@gmail.com>
2017-09-18 10:51:41 -0400

    Added routes

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.json({ type: '*/*'}));
+var router = require('./router');
 
+// Middleware
+app.use(bodyParser.json({ type: '*/*'})); // The "type" is allowing for any type with */*
+router(app);
+
+// Server
 var port = process.env.PORT || 3000;
 
-var server = http.createServer();
 var server = http.createServer(app);
 
 server.listen(port);
