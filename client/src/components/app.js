4d7bbbb957ec91e3833c1097bff98a56c9c72415
Brett Newlund <bmnewlund@gmail.com>
2017-09-19 14:43:01 -0400

    Added NavBar

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import React, { Component } from 'react';
+import { Component } from 'react';
 import NavBarHeader from './nav';
 
 export default class App extends Component {
--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -5,15 +5,29 @@ import {Navbar, Nav, NavItem, NavDropdown, DropdownButton, MenuItem, Collapsible
 	class NavBarHeader extends Component {
 		render() {
 			return (
-				<Navbar>
-					<Navbar.Header>
-						<Navbar.Brand>
-							<a href="#">React-Bootstrap</a>
-						</Navbar.Brand>
-					</Navbar.Header>
-					<nav>
-						<NavItem eventKey={1} href="#">Link</NavItem>
-						<NavItem eventKey={2} href="#"
-					</nav>
+			  <Navbar inverse collapseOnSelect>
+			    <Navbar.Header>
+			      <Navbar.Brand>
+			        <a href="#">React-Bootstrap</a>
+			      </Navbar.Brand>
+			      <Navbar.Toggle />
+			    </Navbar.Header>
+			    <Navbar.Collapse>
+			      <Nav>
+			        <NavItem eventKey={1} href="#">Sign In</NavItem>
+			        <NavItem eventKey={2} href="#">Sign Up</NavItem>
+			        <NavDropdown eventKey={3} title="Cool Stuff" id="basic-nav-dropdown">
+			          <MenuItem eventKey={3.1}>Action</MenuItem>
+			          <MenuItem eventKey={3.2}>Another action</MenuItem>
+			          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+			          <MenuItem divider />
+			          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+			        </NavDropdown>
+			      </Nav>
+			    </Navbar.Collapse>
+			  </Navbar>
+			);
+		}
+	}
 
 	export default NavBarHeader;
